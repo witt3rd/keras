8519a4183c5d3aaa4156d78f052f46cfe0636418
donald <donald@maana.io>
2018-06-18 13:02:35 -0700

    more trianing

--- gym-simple/main.py
+++ gym-simple/main.py
@@ -2,11 +2,15 @@ import gym
 import gym.spaces
 
 env = gym.make('CartPole-v0')
-env.reset()
-
-for _ in range(1000):  # run for 1000 steps
-    env.render()
-    action = env.action_space.sample()  # pick a random action
-    env.step(action)  # take action
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 
 env.close()
