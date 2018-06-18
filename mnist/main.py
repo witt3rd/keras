fa6166e94f3121e1a100734bfd51b7c33021bdaa
donald <donald@maana.io>
2018-06-18 14:29:21 -0700

    split mnist

--- mnist/main.py
+++ mnist/main.py
@@ -71,7 +71,7 @@ print('Test accuracy:', score[1])
 
 model_json = model.to_json()
 
-with open("model_json", "w") as json_file:
+with open("model.json", "w") as json_file:
     json_file.write(model_json)
 
 model.save_weights("model.h5")
