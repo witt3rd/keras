2d1e8fcdcc04fdbca80f20d382e853a05a030170
donald <donald@maana.io>
2018-06-18 15:16:13 -0700

    cleanup gym sample

--- gym-simple/main.py
+++ gym-simple/main.py
@@ -1,10 +1,19 @@
 import gym
 import gym.spaces
 
+# catalog installed environments
 from gym import envs
-print(envs.registry.all())
 
+print("Environments: {}".format(envs.registry.all()))
+
+# load an environment
 env = gym.make('CartPole-v0')
+
+# how are the spaces defined?
+print("Action space: {}".format(env.action_space))
+print("Observation space: {}".format(env.observation_space))
+
+# do stuff
 for i_episode in range(20):
     observation = env.reset()
     for t in range(100):
@@ -16,4 +25,5 @@ for i_episode in range(20):
             print("Episode finished after {} timesteps".format(t+1))
             break
 
+# clean exit
 env.close()
